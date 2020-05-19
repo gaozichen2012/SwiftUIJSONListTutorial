fe87b1f37ff66b02f6dd46686b71426b5a6b8872
gaozichen2012 <gaozichen2012@outlook.com>
2020-05-19 10:57:25 +0800

    晚上回去折腾教程3，再折腾教程2

--- ItunesTaylorSwift/ContentView.swift
+++ ItunesTaylorSwift/ContentView.swift
@@ -22,6 +22,53 @@ struct Result: Codable {
     var collectionName: String
 }
 
+func loadData() {
+    //1.Creating the URL we want to read.
+guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+    print("Invalid URL")
+    return
+}
+//2.Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
+let request = URLRequest(url: url)
+//3.Create and start a networking task from that URL request.
+URLSession.shared.dataTask(with: request) { data, response, error in
+if let data = data {
+    //4.Handle the result of that networking task.
+    if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+        // we have good data – go back to the main thread
+        DispatchQueue.main.async {
+            // update our UI
+            self.results = decodedResponse.results
+        }
+
+        // everything is good, so we can exit
+        return
+    }
+}
+
+// if we're still here it means there was a problem
+print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+}.resume()
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+            .onAppear(perform: loadData)
+        }
+    }
+}
+
+
+
+
 
 //符合Codable协议，以便能够从JSON File解码模型
 //符合Identifiable协议，该协议允许在列表中列出项目。
