0b874158d6899e1a761c5beaa8b4e93ffcc2bd57
Tom Gao <gaozichen2012@outlook.com>
2020-05-29 23:48:15 +0800

    add TMDd app

---
+++ SwiftUITMDbV2-master/README.md
@@ -0,0 +1,14 @@
+# SwiftUI MovieDB TMDb App V2
+
+![Alt text](./promo.png?raw=true "SwiftUI MovieDb with TMDb API iOS App")
+
+This is my second project to create a Movie DB app similar to Apple TV iOS app using TMDb API
+
+## Requirement
+- Xcode 11
+- Register for API Key at https://themoviedb.org
+
+## Getting Started
+- Copy and Clone the project
+- Paste the API Key inside MovieService.swift
+- Build and run the project
---
+++ SwiftUITMDbV2-master/SwiftUIMovieDb/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  SwiftUIMovieDb
+//
+//  Created by Alfian Losari on 23/05/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+//Tabbed View
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            MovieListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "tv")
+                        Text("Movies")
+                    }
+            }
+            .tag(0)
+            
+            MovieSearchView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+            }
+            .tag(1)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
